4ed84b96c717fb9b69150e33463b7f0766cdcd4d
fernandaCoronado <1930013@upv.edu.mx>
2022-02-24 19:43:09 -0600

    Vista cuadrado, sin funcionalidad

---
+++ app/src/main/java/com/example/imath/ui/square/SquareFragment.java
@@ -0,0 +1,56 @@
+package com.example.imath.ui.square;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TabHost;
+
+import com.example.imath.R;
+
+public class SquareFragment extends Fragment {
+
+    private SquareViewModel mViewModel;
+
+    public static SquareFragment newInstance() {
+        return new SquareFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        // return inflater.inflate(R.layout.square_fragment, container, false);
+        View v = inflater.inflate(R.layout.square_fragment, container, false);
+
+        TabHost tabs = v.findViewById(android.R.id.tabhost);
+        tabs.setup();
+
+        TabHost.TabSpec spec = tabs.newTabSpec("pest1");
+        spec.setContent(R.id.pest1);
+        spec.setIndicator("Perímetro", getResources().getDrawable(R.drawable.ic_square));
+        tabs.addTab(spec);
+
+        spec.setContent(R.id.pest2);
+        spec.setIndicator("Área", getResources().getDrawable(R.drawable.ic_square));
+        tabs.addTab(spec);
+
+        tabs.setCurrentTab(0);
+
+        return v;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(SquareViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/imath/ui/square/SquareViewModel.java
@@ -0,0 +1,7 @@
+package com.example.imath.ui.square;
+
+import androidx.lifecycle.ViewModel;
+
+public class SquareViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
