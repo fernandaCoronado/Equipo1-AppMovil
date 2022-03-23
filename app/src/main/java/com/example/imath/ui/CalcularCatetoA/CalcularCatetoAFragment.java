de37232f2bc494659865f8787fc5c2e36499b5b9
fernandaCoronado <1930013@upv.edu.mx>
2022-03-23 00:39:16 -0600

    Cateto a y b

--- app/src/main/java/com/example/imath/ui/CalcularCatetoA/CalcularCatetoAFragment.java
+++ app/src/main/java/com/example/imath/ui/CalcularCatetoA/CalcularCatetoAFragment.java
@@ -11,12 +11,28 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
+import com.example.imath.NumberTextWatcher;
 import com.example.imath.R;
+
+import java.text.DecimalFormat;
 
 public class CalcularCatetoAFragment extends Fragment {
 
     private CalcularCatetoAViewModel mViewModel;
+    private Button btnCalcular, btnBorrar;
+    private EditText txtHipotenusa, txtCatetoB;
+    private TextView tvResultado;
+    private Spinner spUnidad;
+
+    private String unidad;
+    private double hipotenusa, catetoB;
+    private double catetoA;
 
     public static CalcularCatetoAFragment newInstance() {
         return new CalcularCatetoAFragment();
@@ -25,7 +41,80 @@ public class CalcularCatetoAFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.calcular_cateto_a_fragment, container, false);
+        // return inflater.inflate(R.layout.calcular_cateto_a_fragment, container, false);
+        View v = inflater.inflate(R.layout.calcular_cateto_a_fragment, container, false);
+
+        /*
+         * Rescatamos los atributos
+         */
+
+        // Cajas de texto
+        txtHipotenusa = v.findViewById(R.id.id_hipo_a);
+        txtCatetoB = v.findViewById(R.id.id_cateto_b);
+        btnCalcular = v.findViewById(R.id.id_calcular_cata);
+        btnBorrar = v.findViewById(R.id.id_eliminar_cata);
+        tvResultado = v.findViewById(R.id.tvResultado);
+        spUnidad = v.findViewById(R.id.id_unidad);
+
+        // Se agregan automáticamente comas (,) cada mil
+        txtCatetoB.addTextChangedListener(new NumberTextWatcher(txtCatetoB));
+        txtHipotenusa.addTextChangedListener(new NumberTextWatcher(txtHipotenusa));
+
+        // Formato números
+        final DecimalFormat[] decimalFormat = new DecimalFormat[1];
+
+        /*
+         * Funcionamiento de las pantallas
+         */
+
+        String [] unidades = {"Unidad", "cm", "m", "km"};
+        ArrayAdapter<String> uAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_dropdown_item, unidades);
+        spUnidad.setAdapter(uAdapter);
+
+        btnCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                unidad = spUnidad.getSelectedItem().toString();
+                if (txtCatetoB.length() == 0 || txtHipotenusa.length() == 0) {
+                    // txtRadioPerimetro.setError("Faltan datos");
+                    // txtRadioPerimetro.requestFocus();
+                    if (txtCatetoB.length() == 0) {
+                        txtCatetoB.setError("Faltan datos");
+                    }
+                    if (txtHipotenusa.length() == 0) {
+                        txtHipotenusa.setError("Faltan datos");
+                    }
+                } else {
+                    catetoB = Double.parseDouble(txtCatetoB.getText().toString().replace(",", ""));
+                    hipotenusa = Double.parseDouble(txtHipotenusa.getText().toString().replace(",", ""));
+                    if (unidad.equals("cm") || unidad.equals("m") || unidad.equals("km")) {
+                        // Math.sqrt(Math.pow(h, 2) - Math.pow(b, 2));
+                        catetoA = Math.sqrt(Math.pow(hipotenusa, 2) - Math.pow(catetoB, 2));
+                        if (catetoA >= 1000) {
+                            decimalFormat[0] = new DecimalFormat("#0,000.0000");
+                            tvResultado.setText("Cateto a \n" + decimalFormat[0].format(catetoA) + " " + unidad);
+                            //tvResultado.setText("res: " + catetoA + "\ncatb: " + catetoB + "\nhip: " + hipotenusa);
+                        } else {
+                            decimalFormat[0] = new DecimalFormat("#0.0000");
+                            tvResultado.setText("Cateto a \n" + decimalFormat[0].format(catetoA) + " " + unidad);
+                        }
+                    } else {
+                        tvResultado.setText("UNIDAD NO VÁLIDA");
+                    }
+                }
+            }
+        });
+
+        btnBorrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtCatetoB.setText("");
+                txtHipotenusa.setText("");
+                tvResultado.setText("");
+            }
+        });
+
+        return v;
     }
 
     @Override
--- app/src/main/java/com/example/imath/ui/CalcularCatetoB/CalcularCatetoBFragment.java
+++ app/src/main/java/com/example/imath/ui/CalcularCatetoB/CalcularCatetoBFragment.java
@@ -11,12 +11,29 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
+import com.example.imath.NumberTextWatcher;
 import com.example.imath.R;
+
+import java.text.DecimalFormat;
 
 public class CalcularCatetoBFragment extends Fragment {
 
     private CalcularCatetoBViewModel mViewModel;
+
+    private Button btnCalcular, btnBorrar;
+    private EditText txtHipotenusa, txtCatetoA;
+    private TextView tvResultado;
+    private Spinner spUnidad;
+
+    private String unidad;
+    private double hipotenusa, catetoB;
+    private double catetoA;
 
     public static CalcularCatetoBFragment newInstance() {
         return new CalcularCatetoBFragment();
@@ -25,7 +42,80 @@ public class CalcularCatetoBFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.calcular_cateto_b_fragment, container, false);
+        //return inflater.inflate(R.layout.calcular_cateto_b_fragment, container, false);
+
+        View v = inflater.inflate(R.layout.calcular_cateto_b_fragment, container, false);
+
+        /*
+         * Rescatamos los atributos
+         */
+
+        // Cajas de texto
+        txtHipotenusa = v.findViewById(R.id.id_hipo_a);
+        txtCatetoA = v.findViewById(R.id.id_cateto_a);
+        btnCalcular = v.findViewById(R.id.id_calcular_catb);
+        btnBorrar = v.findViewById(R.id.id_eliminar_catb);
+        tvResultado = v.findViewById(R.id.tvResultado);
+        spUnidad = v.findViewById(R.id.id_unidad);
+
+        // Se agregan automáticamente comas (,) cada mil
+        txtCatetoA.addTextChangedListener(new NumberTextWatcher(txtCatetoA));
+        txtHipotenusa.addTextChangedListener(new NumberTextWatcher(txtHipotenusa));
+
+        // Formato números
+        final DecimalFormat[] decimalFormat = new DecimalFormat[1];
+
+        /*
+         * Funcionamiento de las pantallas
+         */
+
+        String [] unidades = {"Unidad", "cm", "m", "km"};
+        ArrayAdapter<String> uAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_dropdown_item, unidades);
+        spUnidad.setAdapter(uAdapter);
+
+        btnCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                unidad = spUnidad.getSelectedItem().toString();
+                if (txtCatetoA.length() == 0 || txtHipotenusa.length() == 0) {
+                    // txtRadioPerimetro.setError("Faltan datos");
+                    // txtRadioPerimetro.requestFocus();
+                    if (txtCatetoA.length() == 0) {
+                        txtCatetoA.setError("Faltan datos");
+                    }
+                    if (txtHipotenusa.length() == 0) {
+                        txtHipotenusa.setError("Faltan datos");
+                    }
+                } else {
+                    catetoA = Double.parseDouble(txtCatetoA.getText().toString().replace(",", ""));
+                    hipotenusa = Double.parseDouble(txtHipotenusa.getText().toString().replace(",", ""));
+                    if (unidad.equals("cm") || unidad.equals("m") || unidad.equals("km")) {
+                        // b = Math.sqrt(Math.pow(h, 2) - Math.pow(a, 2));
+                        catetoB = Math.sqrt(Math.pow(hipotenusa, 2) - Math.pow(catetoA, 2));
+                        if (catetoB >= 1000) {
+                            decimalFormat[0] = new DecimalFormat("#0,000.0000");
+                            tvResultado.setText("Cateto b \n" + decimalFormat[0].format(catetoB) + " " + unidad);
+                        } else {
+                            decimalFormat[0] = new DecimalFormat("#0.0000");
+                            tvResultado.setText("Cateto b \n" + decimalFormat[0].format(catetoB) + " " + unidad);
+                        }
+                    } else {
+                        tvResultado.setText("UNIDAD NO VÁLIDA");
+                    }
+                }
+            }
+        });
+
+        btnBorrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtCatetoA.setText("");
+                txtHipotenusa.setText("");
+                tvResultado.setText("");
+            }
+        });
+
+        return v;
     }
 
     @Override
